3505e304c2562bfe70706e3fd8d33fb11a7cda9f
Alexey <a.gorbunov88@yandex.ru>
2021-10-19 22:51:40 +0300

    start

---
+++ app/src/main/java/com/magistor8/anime/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.magistor8.anime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
+        }
+    }
+}
